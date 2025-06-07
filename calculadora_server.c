bdeaaf849fd3f6da9ad2cc5c13b26d61b7f0af04
TheRoberto219 <99768095+TheRoberto219@users.noreply.github.com>
2025-06-07 16:57:15 -0600

    Update calculadora_server.c

--- calculadora_server.c
+++ calculadora_server.c
@@ -4,16 +4,33 @@
  * as a guideline for developing your own functions.
  */
 
+
 #include "calculadora.h"
+#include <math.h>
 
-float *
-calcular_1_svc(operacion *argp, struct svc_req *rqstp)
-{
-	static float  result;
-
-	/*
-	 * insert server code here
-	 */
-
-	return &result;
+float *calcular_1_svc(operacion *argp, struct svc_req *rqstp) {
+    static float result;
+    
+    switch(argp->operador) {
+        case '+':
+            result = argp->a + argp->b;
+            break;
+        case '-':
+            result = argp->a - argp->b;
+            break;
+        case '*':
+            result = argp->a * argp->b;
+            break;
+        case '/':
+            if(argp->b != 0.0) {
+                result = argp->a / argp->b;
+            } else {
+                result = NAN;
+            }
+            break;
+        default:
+            result = NAN;
+    }
+    
+    return &result;
 }
